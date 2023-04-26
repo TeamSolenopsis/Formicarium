939cf6237ad1fccf68b31dff57539b56f00da6e7
Mart Coppelmans <mart.coppelmans@gmail.com>
2023-04-26 14:21:52 +0200

    lidar implementation and unit test

--- formicarium/formicarium/Lidar.py
+++ formicarium/formicarium/Lidar.py
@@ -1,17 +1,66 @@
 from formicarium.Interfaces import ILidar, IPublisher
-from pygame import Surface
+from pygame import Surface, draw, Color
+import numpy as np
+import math
 
 
 class Lidar(ILidar):
     def __init__(self, range: float, xPos: float, yPos: float, scanPub: IPublisher) -> None:
         super().__init__()
+
+        if range < 0:
+            raise ValueError("Range cannot be negative")
+
+        if xPos < 0:
+            raise ValueError("xPos cannot be negative")
+
+        if yPos < 0:
+            raise ValueError("yPos cannot be negative")
+
+        if scanPub is None:
+            raise ValueError("scanPub cannot be None")
+
         self.range = range
-        self.xPos = xPos
-        self.yPos = yPos
+        self.position = (xPos, yPos)
         self.scanPub = scanPub
+        self.white = Color(255, 255, 255)
+        self.red = Color(255, 0, 0)
 
-    def Scan(self, map: Surface) -> None:
-        pass
+    def Scan(self, map: Surface) -> list[int, int]:
+        if map is None:
+            raise ValueError("map cannot be None")
 
-    def SetPosition(self, x: float, y: float) -> None:
-        pass
+        data = []
+        x_0, y_0 = self.position[0], self.position[1]
+        for angle in np.linspace(0, 2*math.pi, 60, False):
+            # get distance at current angle
+            x_i, y_i = (x_0 + self.range * math.cos(angle)), (y_0 - self.range * math.sin(angle))
+
+            # find endpoint
+            for i in range(15, 100):
+                j = i/100
+                x_t = int(x_i * j + x_0 * (1 - j))
+                y_t = int(y_i * j + y_0 * (1 - j))
+                if not (0 <= x_t < map.get_width() and 0 <= y_t < map.get_height()):
+                    continue
+
+                color = map.get_at((x_t, y_t))
+                if color[:-1] != self.white:
+                    data.append((x_t, y_t))
+                    break
+
+            if (i != 99):
+                draw.line(map, self.red, (x_0, y_0), (x_t, y_t))
+
+        return data
+
+    def SetPosition(self, x: float, y: float) -> tuple[float, float]:
+        if x < 0:
+            raise ValueError("x cannot be negative")
+
+        if y < 0:
+            raise ValueError("y cannot be negative")
+
+        self.position = (x, y)
+
+        return self.position
--- formicarium/test/UnitTest/lidar_unit_test.py
+++ formicarium/test/UnitTest/lidar_unit_test.py
@@ -3,9 +3,58 @@ from unittest import TestCase
 
 
 class LidarUnitTest(TestCase):
-    def test_create_lidar(self):
+    def test_create_lidar_nothrow(self):
         try:
             lidar = Lidar.Lidar(1, 1, 1, 1)
             self.assertIsNotNone(lidar)
         except Exception as e:
             assert False, f"Unexpected exception: {e}"
+
+    def test_create_lidar_with_negative_range_throw(self):
+        with self.assertRaises(ValueError):
+            Lidar.Lidar(-1, 1, 1, 1)
+
+    def test_create_lidar_with_negative_xPos_throw(self):
+        with self.assertRaises(ValueError):
+            Lidar.Lidar(1, -1, 1, 1)
+
+    def test_create_lidar_with_negative_yPos_throw(self):
+        with self.assertRaises(ValueError):
+            Lidar.Lidar(1, 1, -1, 1)
+
+    def test_create_lidar_with_none_scanPub_throw(self):
+        with self.assertRaises(ValueError):
+            Lidar.Lidar(1, 1, 1, None)
+
+    def test_scan_env_is_none_throw(self):
+        lidar = Lidar.Lidar(1, 1, 1, 1)
+
+        with self.assertRaises(ValueError):
+            lidar.Scan(None)
+
+    def test_set_position_with_negative_x_throw(self):
+        lidar = Lidar.Lidar(1, 1, 1, 1)
+
+        with self.assertRaises(ValueError):
+            lidar.SetPosition(-1, 1)
+
+    def test_set_position_with_negative_y_throw(self):
+        lidar = Lidar.Lidar(1, 1, 1, 1)
+
+        with self.assertRaises(ValueError):
+            lidar.SetPosition(1, -1)
+
+    def test_set_position_with_valid_x_y_return_x_y(self):
+        lidar = Lidar.Lidar(1, 1, 1, 1)
+
+        x, y = lidar.SetPosition(2, 2)
+
+        self.assertEqual(x, 2)
+        self.assertEqual(y, 2)
+
+    def test_scan_with_valid_return_data(self):
+        lidar = Lidar.Lidar(1, 1, 1, 1)
+        from pygame import Surface
+        env = Surface((1, 1))
+
+        self.assertIsNotNone(lidar.Scan(env))
