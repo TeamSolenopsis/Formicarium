9cfcf57507f2c5eadb1a2e4d66f87fac864ac77d
Mart Coppelmans <mart.coppelmans@gmail.com>
2023-05-08 12:07:21 +0200

    review

--- formicarium/formicarium/RobotConfig.py
+++ formicarium/formicarium/RobotConfig.py
@@ -3,4 +3,7 @@ from pygame import image
 
 image = image.load(f'{getcwd()}/src/Formicarium/formicarium/robot.png')
 WheelBase = image.get_width()
-WheelRadius = (image.get_height() / 3) / 2
+divosor = 3
+WheelDiameter = image.get_height() / divosor
+WheelRadius = WheelDiameter / 2
+
