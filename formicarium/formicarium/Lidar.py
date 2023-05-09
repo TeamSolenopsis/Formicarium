1fb07da342d4c30820f4ea88ab00ca736288f0b0
Mart Coppelmans <mart.coppelmans@gmail.com>
2023-05-09 15:55:24 +0200

    Restructured classes so node is passed to robot

--- formicarium/formicarium/Lidar.py
+++ formicarium/formicarium/Lidar.py
@@ -1,28 +1,23 @@
-from formicarium.Interfaces import ILidar, IPublisher, ICollider
+from formicarium.Interfaces import ILidar, ICollider
 from pygame import Surface, draw, Color
 import numpy as np
 import math
 from sensor_msgs.msg import LaserScan
 
-
 class Lidar(ILidar):
-    def __init__(self, collider:ICollider, range: float, xPos: float, yPos: float, scanPub: IPublisher) -> None:
+    def __init__(self, collider:ICollider, range: float, xPos: float, yPos: float) -> None:
         super().__init__()
 
         if range < 0:
             raise ValueError("Range cannot be negative")
 
-        if scanPub is None:
-            raise ValueError("scanPub cannot be None")
-
         self.collider = collider
         self.range = range
         self.position = (xPos, yPos)
-        self.scanPub = scanPub
         self.white = Color(255, 255, 255)
         self.red = Color(255, 0, 0)
 
-    def Scan(self, map: Surface) -> LaserScan:
+    def scan(self, map: Surface) -> LaserScan:
         if map is None:
             raise ValueError("map cannot be None")
 
@@ -65,7 +60,7 @@ class Lidar(ILidar):
 
         return msg
 
-    def SetPosition(self, x: float, y: float) -> tuple[float, float]:
+    def set_position(self, x: float, y: float) -> tuple[float, float]:
         self.position = (x, y)
 
         return self.position
