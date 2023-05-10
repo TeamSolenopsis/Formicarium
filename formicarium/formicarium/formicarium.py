627e92713489e84339a970f0ae5887887280cc0b
CasLeeflang <466933@student.fontys.nl>
2023-05-10 11:28:19 +0200

    FOR-16 Spawn objects during run time

--- formicarium/formicarium/formicarium.py
+++ formicarium/formicarium/formicarium.py
@@ -9,7 +9,7 @@ import formicarium.DiffRobot as DiffRobot
 import formicarium.Lidar as Lidar
 import formicarium.RobotConfig as RobotConfig
 import formicarium.Environment as Environment
-from formicarium_interfaces.srv import Spawner
+from formicarium_interfaces.srv import Spawner, Obstacle
 
 class Formicarium(Node):
     def __init__(self):
@@ -19,17 +19,18 @@ class Formicarium(Node):
         self.environment_height = 800
         self.environment = Environment.Environment(self.environment_width, self.environment_height)
         self.spawn_serv = self.create_service(Spawner, 'spawn', self.spawn)
+        self.spawn_obstacle_serv = self.create_service(Obstacle, 'spawn_obstacle', self.spawn_obstacle)
         self.timer = self.create_timer(1.0 / 30.0, self.update)
-
-        #TODO This should be moved to a spawn_obstacle function and called with a service command
-        #Parameter is a position and dimension tuple
-        self.environment.add_obstacle((300, 300, 300, 300))
 
     def spawn(self, request, response):
         response.robot_names = self.environment.get_robot_names()
 
-        if not self.validate_spawn_pose(request.x, request.y) or request.robot_name in response.robot_names:
+        if not self.validate_spawn_pose(request.x, request.y):
             response.error_message = f'Start position {request.x, request.y} is out of bounds for robot {request.robot_name}'
+            return response
+        
+        if request.robot_name in response.robot_names:
+            response.error_message = f'Robot name {request.robot_name} already exists'
             return response
         
         lidar = Lidar.Lidar(self.environment, 500, request.x, request.y)
@@ -37,12 +38,19 @@ class Formicarium(Node):
                                     request.x, request.y, lidar, RobotConfig.image, self.environment, self)
         self.environment.add_robot(robot)
         return response
+    
+    def spawn_obstacle(self, request, response):       
+        self.environment.add_obstacle((request.x, request.y, request.width, request.height))
+        return response
 
     def update(self):
         self.environment.update()
 
     def validate_spawn_pose(self, x:float, y:float):
         return RobotConfig.Width / 2 < x and x < (self.environment_width - (RobotConfig.Width / 2)) and RobotConfig.Height / 2 < y and y < (self.environment_height - (RobotConfig.Height / 2))
+    
+    def validate_spawn_obstacle_pose(self, x, y, width, height):
+        return (x + width) < self.environment_width and (y + height) < self.environment_height and x > 0 and y > 0
     
 def main(args=None):
     rclpy.init(args=args)
